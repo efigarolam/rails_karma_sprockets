9605eaad884a80af631b08948dda2f4d8570b3c7
Eduardo Figarola <eduardo.figarola@crowdint.com>
2015-06-04 10:27:23 -0500

    Remove tests warnings

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 require "rails_karma_sprockets"
@@ -20,7 +20,7 @@ module Dummy
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
 
